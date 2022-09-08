e45abdd1069fdaf23da318aeff3633ce1aee8c88
Debbie Wright <debbiewright@live.co.uk>
2022-09-08 14:42:48 +0100

    Functionality added to allow players to choose their own squares

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ const TicTacToe = (() => {
          
         const gameBoard = (function() {
             'use strict';
-            let board = ["O", "X", "X", "X", "O","O","X","O","X"]; 
-            const updateBoard = (a, b) => a - b;
-            const resetBoard = (a, b) => a * b;
+            let board = ["", "", "", "", "","","","",""]; 
+            let turn = "";
 
-            function createBoard(){
+            function updateBoard(){
                 for(let i=0; i<9; i++){
                     let gameSquare = document.getElementById(i);
                     gameSquare.innerHTML = board[i];
                 }
             }
 
+            function addToBoard(i){
+                board[i] = turn;
+                updateBoard();
+            }
+
+            function getPlayerTurn(){
+                return turn;
+            }
+            function setPlayerTurn(theTurn){
+                turn = theTurn;
+            }
+
             return {
-                createBoard: createBoard
+                updateBoard: updateBoard,
+                addToBoard: addToBoard,
+                getPlayerTurn: getPlayerTurn,
+                setPlayerTurn: setPlayerTurn
             };
             
         })();
@@ -25,19 +39,26 @@ const TicTacToe = (() => {
             let score = 0;
             let characterSymbol = XorO;
             let playerName = name; 
+            let isTurn = false;
             
             return{
-                get name(){
+                getname(){
                     return playerName;
                 },
-                set name(name){
+                setname(name){
                     playerName = name;
                 },
-                get characterSymbol(){
+                getcharacterSymbol(){
                     return XorO;
                 },
-                set characterSymbol(XorO){
+                setcharacterSymbol(XorO){
                     characterSymbol = XorO;
+                },
+                getTurn(){
+                    return isTurn;
+                },
+                setTurn(theTurn){
+                    isTurn = theTurn;
                 }
 
             }
@@ -50,17 +71,39 @@ const TicTacToe = (() => {
                 let player1 = playerFactory("Aaron", "X");
                 let player2 = playerFactory("Bob", "O");
 
-                let turn = player1.characterSymbol;
+                player1.setTurn(true);
+                gameBoard.setPlayerTurn(player1.getcharacterSymbol());
+                console.log("The game board current symbol is: " + gameBoard.getPlayerTurn());
 
                 // Add event listeners to each square
                 for(let i=0; i<9; i++){
                     let gameSquare = document.getElementById(i);
-                    gameSquare.addEventListener("click", function(i){
+                    gameSquare.addEventListener("click", function(){
+                        console.log("i: " + i);
                         gameBoard.addToBoard(i);
+                        changeTurn();
+                        console.log("The game board current symbol is: " + gameBoard.getPlayerTurn());
                     });
                 }
 
-                gameBoard.createBoard();
+                function changeTurn(){
+                    if(player1.getTurn()){
+                        player2.setTurn(true);
+                        player1.setTurn(false);
+                        let player2Symbol = player2.getcharacterSymbol();
+                        console.log("player2Symbol " + player2Symbol);
+                        gameBoard.setPlayerTurn(player2Symbol);
+                    }else{
+                        player2.setTurn(false);
+                        player1.setTurn(true);
+                        let player1Symbol = player1.getcharacterSymbol();
+                        console.log("player1Symbol " + player1Symbol);
+                        gameBoard.setPlayerTurn(player1Symbol);
+                    }
+                }
+
+                
+                //gameBoard.createBoard();
             
             
         })();
